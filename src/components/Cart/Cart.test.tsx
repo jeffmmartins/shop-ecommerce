e9c1d82e3e5c472108a2dd8c3dbcef9ce47f535c
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-25 20:53:10 -0300

    feat: inserindo o provider no teste

--- src/components/Cart/Cart.test.tsx
+++ src/components/Cart/Cart.test.tsx
@@ -1,8 +1,14 @@
 import { render } from "@testing-library/react"
 import { Cart } from "./Cart"
+import { Provider } from "react-redux"
+import { store } from "../../redux/store"
+
 
 describe('Cart > Unit Test', () => {
     it('Should render an empty cart correctly', () => {
-        render(<Cart  showCart={true}/>) // indica se o cart deve ser mostrado ou não. 
+        render(
+        <Provider store={store}>
+         <Cart  showCart={true}/>
+        </Provider>) // indica se o cart deve ser mostrado ou não. 
     })
 })
